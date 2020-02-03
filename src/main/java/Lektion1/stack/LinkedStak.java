c9810ce312165e8a8245d4072b5b1026421a63c9
Mark-Laptop <bluedogdk@gmail.com>
2020-02-03 15:04:22 +0100

    Tilføjet Stak klasser og interface

---
+++ src/main/java/Lektion1/stack/LinkedStak.java
@@ -0,0 +1,21 @@
+package Lektion1.stack;
+
+public class LinkedStak implements Stak{
+    public void push(String e){
+
+    }
+    public String pop(){
+
+        return "abc";
+    }
+
+    public boolean isEmpty(){
+        return true;
+    }
+    public boolean isFull(){
+        return true;
+    }
+    public void show(){
+
+    }
+}
---
+++ src/main/java/Lektion1/stack/Stak.java
@@ -0,0 +1,9 @@
+package Lektion1.stack;
+
+interface Stak {
+    void push(String e);
+    String pop();
+    boolean isEmpty();
+    boolean isFull();
+    void show();
+}
