2809220bb0bbc6742b1c6303a65c68dd831efdf0
Mark-Laptop <bluedogdk@gmail.com>
2020-02-03 15:39:16 +0100

    LinkedStak kan nu indsætte elementer, poppe og printe

--- src/main/java/Lektion1/stack/LinkedStak.java
+++ src/main/java/Lektion1/stack/LinkedStak.java
@@ -1,21 +1,41 @@
 package Lektion1.stack;
 
 public class LinkedStak implements Stak{
-    public void push(String e){
+    Object ptr;
+    public LinkedStak(){
+    ptr = null;
+    }
 
+    public void push(String e){
+        if(ptr == null){
+            ptr = new Object(e,null);
+        }else{
+            ptr = new Object(e,ptr);
+        }
     }
     public String pop(){
-
-        return "abc";
+        String out = ptr.input;
+        ptr = ptr.ptr;
+        return out;
     }
 
     public boolean isEmpty(){
-        return true;
+        return (ptr == null);
     }
     public boolean isFull(){
-        return true;
+        return false;
     }
     public void show(){
-
+        if (ptr == null){
+            System.out.println("---End---");
+            return;
+        }
+        Object temp = ptr;
+        while (temp.ptr != null){
+            System.out.println(temp.input);
+            temp=temp.ptr;
+        }
+        System.out.println(temp.input);
+        System.out.println("---End---");
     }
 }
---
+++ src/main/java/Lektion1/stack/MainStak1.java
@@ -0,0 +1,10 @@
+package Lektion1.stack;
+
+public class MainStak1 {
+    public static void main(String[] args) {
+        LinkedStak linkedStak = new LinkedStak();
+        linkedStak.push("Test");
+        linkedStak.show();
+        linkedStak.pop();
+    }
+}
