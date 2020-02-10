01286e848eb1a4c6bc62fc40b7b5c0a689409b72
Mark-Laptop <bluedogdk@gmail.com>
2020-02-10 13:33:46 +0100

    Main metode sat op

--- src/main/java/Lektion2/Main1.java
+++ src/main/java/Lektion2/Main1.java
@@ -1,4 +1,11 @@
 package Lektion2;
 
 public class Main1 {
+    public static void main(String[] args) {
+        IData data = new DemoData();
+        IUI ui = new TUI(data);
+        while (true){
+            ui.showMenu();
+        }
+    }
 }
