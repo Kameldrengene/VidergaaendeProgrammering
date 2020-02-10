356ddfdefc87c6e497362b194928f80133358439
Mark-Laptop <bluedogdk@gmail.com>
2020-02-10 13:36:19 +0100

    Tilføjet TUI

---
+++ src/main/java/Lektion2/TUI.java
@@ -0,0 +1,35 @@
+package Lektion2;
+
+public class TUI implements IUI {
+
+    IData data;
+
+    public TUI (IData data){
+        this.data = data;
+    }
+
+    @Override
+    public void showMenu() {
+
+    }
+
+    @Override
+    public void showIngredients() {
+
+    }
+
+    @Override
+    public void showIngredient() {
+
+    }
+
+    @Override
+    public void changeIngredient() {
+
+    }
+
+    @Override
+    public void createIngredient() {
+
+    }
+}
