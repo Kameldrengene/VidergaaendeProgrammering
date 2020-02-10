5b8ca58d81148d632ed43a0100a898ad0aa986c0
Mark-Laptop <bluedogdk@gmail.com>
2020-02-10 13:30:55 +0100

    Tilføjet metoder fra interface

--- src/main/java/Lektion2/DemoData.java
+++ src/main/java/Lektion2/DemoData.java
@@ -1,6 +1,39 @@
 package Lektion2;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class DemoData implements IData {
+    @Override
+    public String[] getAllIngredients() {
+        return new String[0];
+    }
+
+    @Override
+    public String getIngredientName(int id) {
+        return null;
+    }
+
+    @Override
+    public int getIngredientAmount(int id) {
+        return 0;
+    }
+
+    @Override
+    public void setIngredientName(int id, String name) {
+
+    }
+
+    @Override
+    public void setIngredientAmount(int id, int amount) {
+
+    }
+
+    @Override
+    public void createIngredient(int id, String name, int amount) {
+
+    }
+
     private class Ingredient {
         int id;
         String name;
@@ -13,6 +46,7 @@ public class DemoData implements IData {
         @Override
         public String toString() {
             //TODO: lav toString metode
+            return null;
         }
     }
 
