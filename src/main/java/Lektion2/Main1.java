d43dc9e5f4886fef8e71ba5281a726e2a1d239ed
Mark-Laptop <bluedogdk@gmail.com>
2020-02-10 14:22:36 +0100

    2 lags done

--- src/main/java/Lektion2/Main1.java
+++ src/main/java/Lektion2/Main1.java
@@ -1,19 +1,19 @@
 package Lektion2;
 
 public class Main1 {
-//    public static void main(String[] args) {
-//        IData data = new DemoData();
-//        IUI ui = new TUI(data);
-//        while (true){
-//            ui.showMenu();
-//        }
-//    }
-
     public static void main(String[] args) {
-        DemoData demoData = new DemoData();
-        for(int i = 0; i < demoData.getAllIngredients().length;i++){
-            System.out.println(demoData.getAllIngredients()[i]);
+        IData data = new DemoData();
+        IUI ui = new TUI(data);
+        while (true){
+            ui.showMenu();
         }
-
     }
+
+//    public static void main(String[] args) {
+//        DemoData demoData = new DemoData();
+//        for(int i = 0; i < demoData.getAllIngredients().length;i++){
+//            System.out.println(demoData.getAllIngredients()[i]);
+//        }
+//
+//    }
 }
